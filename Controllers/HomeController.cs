945856ca08ddabc45cf73177f6cafdd338824f85
pepe <pepe@DESKTOP-FVE60BB>
2020-11-18 12:56:23 +0100

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using OnlineShoppingStore.Models.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HomeIndexViewModel model = new HomeIndexViewModel();
+            return View(model.CreateModel());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace OnlineShoppingStore.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        
+        public IEnumerable<Tbl_Product> ListOfProducts { get; set; }
+        public HomeIndexViewModel CreateModel()
+        {
+             
+            return new HomeIndexViewModel
+            {
+                ListOfProducts = _unitOfWork.GetRepositoryInstance<Tbl_Product>().GetAllRecords()
+            };
+        }
+    }
+}
